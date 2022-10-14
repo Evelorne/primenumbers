6acdec0075d7228df04dbaf6178899c5f6e2763a
Evelorne <tayfundaldik@gmail.com>
2022-10-14 11:16:03 +0300

    reverse

--- src/reverse.java
+++ src/reverse.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class reverse {
+import java.util.Scanner;
+
+
+public class reverse {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        n=sc.nextInt();
+
+        for(int i=0; i<n; i++){
+
+            for(int k=0; k<i; k++){
+                System.out.print(" ");
+            }for(int j=0; j<(2*n)-1-(2*i); j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
